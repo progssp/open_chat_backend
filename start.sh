7fbe4762c51fcbbc76d0837ffc8c3d10de604476
progssp <shantanusingh922@gmail.com>
2026-06-03 16:07:58 +0530

    start.sh changed for optimizing app added list files

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ set -e
 # run migrations
 php /var/www/html/artisan migrate --force || echo "migration err $?"
 
+ls -l /var/www/html
+ls -l /var/www/html/public
+
 
 echo "launching web server and queue workers"
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
