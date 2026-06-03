d77b34be675534b4c2238a8e32167c7bac55f51e
progssp <shantanusingh922@gmail.com>
2026-06-03 16:00:49 +0530

    start.sh changed for optimizing app removed db:seed

--- start.sh
+++ start.sh
@@ -3,10 +3,6 @@ set -e
 
 # run migrations
 php /var/www/html/artisan migrate --force || echo "migration err $?"
-php /var/www/html/artisan db:seed || echo "db:seed err $?"
-php /var/www/html/artisan passport:keys --force || echo "passport:keys err $?"
-php /var/www/html/artisan passport:client --personal --no-interaction || echo "passport:client err $?"
-php /var/www/html/artisan storage:link || echo "storage:link err $?"
 
 
 echo "launching web server and queue workers"
